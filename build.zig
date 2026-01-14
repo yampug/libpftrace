3812f181dd82b7e682b5f9777bad123175569dd3
yampug <bpsekii@gmail.com>
2026-01-14 09:09:31 +0100

    Enable PIC for static library

--- build.zig
+++ build.zig
@@ -9,6 +9,7 @@ pub fn build(b: *std.Build) void {
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
+        .pic = true,
     });
 
     // Library: libpftrace
